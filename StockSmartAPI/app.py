0fdb586264910fd7f8b7dc78da2bcbb27bec33c1
AARON <aaron7cas@gmail.com>
2024-07-15 20:07:29 +0200

    POST creado y cambios menores

--- StockSmartAPI/app.py
+++ StockSmartAPI/app.py
@@ -1,8 +1,8 @@
-import os
-from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from azure.cosmos import CosmosClient
 
+import os
+from dotenv import load_dotenv
 
 # Creamos una instancia de Flask
 app = Flask(__name__)   #Equiparable a escribir lo que contiene esa variable, que es __main__
@@ -21,6 +21,7 @@ cosmosClient = CosmosClient(endPoint, key)
 db = cosmosClient.get_database_client(dbName)
 container = db.get_container_client(containerName)
 
+
 ################################
 # Rutas de la aplicación Flask #
 ################################
@@ -28,59 +29,62 @@ container = db.get_container_client(containerName)
 # Ruta: http://dominio.com/productos    
 @app.route("/productos", methods=["GET"])          
 def products_get():
-
+    
     try:
-        items = list(container.read_all_items())   # Probar y decidir si lo dejamos así o como query = "SELECT * FROM c"
+        items = list(container.read_all_items())
         return jsonify(items)
 
     except Exception as e:
-        print(f"Error al obtener los productos: {str(e)}")
-        return jsonify({"error": "Error al obtener los productos"}),
-   
+        print(f"Error al mostrar productos: {str(e)}")
+        return jsonify({"error": "Error al mostrar productos"}), 404    
 
+    
 # Ruta: http://dominio.com/productos/34                         
 @app.route("/productos/<int:id>", methods=["GET"])
 def product_get(id):
-
+    
     try:
-        # Obtener el producto por ID desde CosmosDB
         query = f"SELECT * FROM c WHERE c.ProductID = '{id}'"
         items = list(container.query_items(query, enable_cross_partition_query=True))  # Hay que habilitar las consultas entre particiones cruzadas
-        
+            
         return jsonify(items)  # Devolver el primer producto encontrado como JSON
-
+    
     except Exception as e:
-        print(f"Error al obtener el producto con ID {id}: {str(e)}")
-        return jsonify({"error": "Error al obtener el producto"}), 500
+        print(f"Error al mostrar producto: {str(e)}")
+        return jsonify({"error": "Error al mostrar producto"}), 404
+   
         
 # Ruta: http://dominio.com/productos     
 @app.route("/productos", methods=["POST"])
 def products_post():
 
     try:
-        # Obtención de los datos del producto desde el cuerpo de la solicitud
+        # Obtener los datos del producto desde el cuerpo de la solicitud
         product_data = request.get_json()
 
         # Insertar el nuevo producto en Cosmos DB
         container.create_item(body=product_data)
 
         return jsonify({"message": "Producto creado exitosamente"}), 201
-    
+
     except Exception as e:
         print(f"Error al insertar el producto: {str(e)}")
         return jsonify({"error": "Error al insertar el producto"}), 400
 
+
 # Ruta: http://dominio.com/productos     
 @app.route("/productos/<id>", methods=["PUT"])
 def products_put(id):
-
+    
+    
     
 
 # Ruta: http://dominio.com/productos/34                    
 @app.route("/productos/<id>", methods=["DELETE"])
 def products_delete(id):
     
-
+    
+        
 
 ################################################################
 # Ejecutar la aplicación de Flask en el servidor web integrado #
